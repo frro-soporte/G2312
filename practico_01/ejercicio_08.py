cd84edb8d7009c032da71f78c4f3556ce7dc05e3
Matias Bais <34136926+MatiasBais@users.noreply.github.com>
2023-04-14 15:57:24 -0300

    Update ejercicio_08.py
    falta el ultimo

--- practico_01/ejercicio_08.py
+++ practico_01/ejercicio_08.py
@@ -6,10 +6,12 @@ from typing import Any, Iterable
 def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Toma dos listas y devuelve un booleano en base a si tienen al menos 1
     elemento en común.
-
     Restricción: Utilizar bucles anidados.
     """
-    pass # Completar
+    for elem in lista_1:
+      for elem2 in lista_2:
+        return elem==elem2
+    return False
 
 
 # NO MODIFICAR - INICIO
@@ -24,7 +26,7 @@ assert not superposicion_basico(test_list, (2, "world", 30.85))
 
 def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando un sólo bucle y el operador IN."""
-    pass # Completar
+    return any(elem in lista_2 for elem in lista_1)
 
 
 # NO MODIFICAR - INICIO
@@ -41,7 +43,7 @@ def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando sin bucles, el operador in y la funcion any.
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
-    pass # Completar
+    return any(elem in lista_2 for elem in lista_1)
 
 
 # NO MODIFICAR - INICIO
