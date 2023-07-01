c52b7fe6cabeed66f114697873004a595fad0095
Tadeo Schlieper <tadeo.sch@hotmail.com>
2023-07-01 14:14:52 -0300

    Update practico_04: 03, 04

--- practico_04/ejercicio_03.py
+++ practico_04/ejercicio_03.py
@@ -1,6 +1,7 @@
 """Base de Datos SQL - Baja"""
 
 import datetime
+import sqlite3
 
 from practico_04.ejercicio_01 import reset_tabla
 from practico_04.ejercicio_02 import agregar_persona
@@ -10,7 +11,16 @@ def borrar_persona(id_persona):
     """Implementar la funcion borrar_persona, que elimina un registro en la 
     tabla Persona. Devuelve un booleano en base a si encontro el registro y lo 
     borro o no."""
-    pass # Completar
+    con = sqlite3.connect("practico4.db")
+    cur = con.cursor()
+    try:
+        cur.execute("DELETE FROM persona WHERE id_persona = ?", (id_persona,))
+        con.commit()
+        return cur.rowcount == 1
+    finally:
+        cur.close()
+        con.close()
+
 
 # NO MODIFICAR - INICIO
 @reset_tabla
--- practico_04/ejercicio_04.py
+++ practico_04/ejercicio_04.py
@@ -1,6 +1,7 @@
 """Base de Datos SQL - Búsqueda"""
 
 import datetime
+import sqlite3
 
 from practico_04.ejercicio_01 import reset_tabla
 from practico_04.ejercicio_02 import agregar_persona
@@ -11,7 +12,41 @@ def buscar_persona(id_persona):
     persona basado en su id. El return es una tupla que contiene sus campos: 
     id, nombre, nacimiento, dni y altura. Si no encuentra ningun registro, 
     devuelve False."""
-    pass # Completar
+    con = sqlite3.connect(
+        "practico4.db",
+        detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+    )
+    cur = con.cursor()
+    try:
+        formatos_fecha = [
+            "date",  # date -> fecha (datetime.date)
+            "timestamp",  # timestamp -> fecha y hora (datetime.datetime)
+        ]
+
+        persona = None
+        for i, formato in enumerate(formatos_fecha, 1):
+            try:
+                cur.execute(
+                    f"""SELECT
+                    id_persona,
+                    nombre,
+                    fecha_nacimiento as "fecha_nac [{formato}]",
+                    dni,
+                    altura
+                    FROM persona WHERE id_persona = ?""",
+                    (id_persona,)
+                )
+                persona = cur.fetchone()
+                break
+            except ValueError as e:
+                if i < len(formatos_fecha):
+                    pass  # Error con el formato del campo; probar siguiente formato
+                else:
+                    raise e
+        return persona or False
+    finally:
+        cur.close()
+        con.close()
 
 
 # NO MODIFICAR - INICIO
