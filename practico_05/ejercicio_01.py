27ca52b0b053cac7591d0c325cca721f8b39f17b
Tadeo Schlieper <tadeo.sch@hotmail.com>
2023-11-08 13:27:40 -0300

    Update practico_05: 01, 02

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -1,19 +1,27 @@
 """Base de Datos - Creación de Clase en ORM"""
 
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
+
 
 class Socio(Base):
     """Implementar un modelo Socio a traves de Alchemy que cuente con los siguientes campos:
-        - id_socio: entero (clave primaria, auto-incremental, unico)
+        - id: entero (clave primaria, auto-incremental, unico)
         - dni: entero (unico)
         - nombre: string (longitud 250)
         - apellido: string (longitud 250)
     """
-    __tablename__ = 'socios'
+    __tablename__ = 'socio'
 
-    # Completar
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    dni: Mapped[int] = mapped_column(unique=True)
+    nombre: Mapped[str] = mapped_column(String(250))
+    apellido: Mapped[str] = mapped_column(String(250))
 
+    def __repr__(self) -> str:
+        return f"Socio(id={self.id!r}, dni={self.dni!r}, nombre={self.nombre!r}, apellido={self.apellido!r})"
--- practico_05/ejercicio_02.py
+++ practico_05/ejercicio_02.py
@@ -1,58 +1,103 @@
 """Base de Datos - ORM"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from ejercicio_01 import Base, Socio
+from sqlalchemy import create_engine, select, delete, func, ScalarResult
+from sqlalchemy.orm import Session
+from practico_05.ejercicio_01 import Base, Socio
 
-from typing import List, Optional
+from typing import List, Optional, cast
 
-class DatosSocio():
+
+class DatosSocio:
 
     def __init__(self):
-        pass # Completar
+        self._engine = create_engine("sqlite+pysqlite:///practico5.db")
+        Base.metadata.create_all(self._engine)
+        self._sess = Session(self._engine)
+
+    def __del__(self):  # se ejecuta siempre que termine la ejecución, incluso por error
+        self._sess.rollback()
+        if self.borrar_todos():
+            print("fin - todos los socios borrados")
+        else:
+            print("fin - no se pudieron borrar socios")
 
     def buscar(self, id_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su id. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
+        soc: Optional[Socio] = self._sess.get(Socio, id_socio)
+        return soc
 
     def buscar_dni(self, dni_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su dni. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
+        soc: Optional[Socio] = self._sess.scalar(
+            select(Socio)
+            .where(Socio.dni == dni_socio)
+        )
+        return soc
         
     def todos(self) -> List[Socio]:
         """Devuelve listado de todos los socios en la base de datos."""
-        pass # Completar
+        res: ScalarResult[Socio] = self._sess.scalars(select(Socio))
+        return list(res)
 
     def borrar_todos(self) -> bool:
         """Borra todos los socios de la base de datos. Devuelve True si el 
         borrado fue exitoso.
         """
-        pass # Completar
+        self._sess.execute(delete(Socio))
+        self._sess.commit()
+        return True
 
     def alta(self, socio: Socio) -> Socio:
         """Agrega un nuevo socio a la tabla y lo devuelve"""
-        pass # Completar
+        self._sess.add(socio)
+        self._sess.commit()
+        return socio
 
     def baja(self, id_socio: int) -> bool:
         """Borra el socio especificado por el id. Devuelve True si el borrado 
         fue exitoso.
         """
-        pass # Completar
+        soc = self.buscar(id_socio)
+        if soc is None:
+            raise ValueError()  # no existe socio con id={id_socio}. no se puede borrar
+        self._sess.delete(soc)
+        self._sess.commit()
+        return True
 
     def modificacion(self, socio: Socio) -> Socio:
         """Guarda un socio con sus datos modificados. Devuelve el Socio 
         modificado.
         """
-        pass # Completar
+        with self._sess.no_autoflush:  # para consultar la base de datos sin hacer update
+            soc_por_id = self.buscar(socio.id)
+            # se validan casos no cubiertos en los test
+            if soc_por_id is None:  # el socio no fue creado en la base de datos
+                raise ValueError(f"No existe socio con id={socio.id} en tabla {Socio.__tablename__!r}")
+            soc_por_dni = self.buscar_dni(socio.dni)
+            if soc_por_dni is not None and soc_por_dni.id != socio.id:  # el dni a guardar es de otro socio
+                # las 3 lineas siguientes: guarda el socio modificado a la vez que recupera el original
+                # de la base de datos (posiblemente (soc_por_id is socio) == True) para lanzar error descriptivo
+                modificado = Socio(id=socio.id, dni=socio.dni, nombre=socio.nombre, apellido=socio.apellido)
+                self._sess.rollback()
+                raise ValueError(
+                    f"Ya existe socio con dni={soc_por_dni.dni} en tabla {Socio.__tablename__!r}. No se puede aplicar"
+                    f" la modificación del socio: {soc_por_id} => {modificado}"
+                )
+            if soc_por_id is not socio:
+                soc_por_id.dni = socio.dni
+                soc_por_id.nombre = socio.nombre
+                soc_por_id.apellido = socio.apellido
+        self._sess.commit()
+        return soc_por_id
     
-    def contarSocios(self) -> int:
+    def contar_socios(self) -> int:
         """Devuelve el total de socios que existen en la tabla"""
-        pass # Completar
-
+        cantidad: int = self._sess.scalar(func.count(Socio.id))
+        return cantidad
 
 
 # NO MODIFICAR - INICIO
